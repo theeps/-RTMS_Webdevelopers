f79246e7eda84f6465c34a06e1dd8fa823e266c0
miznath <ahmadhhisham98@gmail.com>
2021-11-11 18:20:32 +0530

    First commit

---
+++ quiz/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+#from quiz.models import Exam
+
+# Create your views here.
+#def home(request):
+    #return HttpResponse("Hello")
+
+#def home(request):
+    #exam = Exam.objects.all()
+    #return render(request,"home.html",{"exam":exam})
+
