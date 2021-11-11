d85d5cbd440398c20f2ec4cd33521250fd96a0e0
miznath <ahmadhhisham98@gmail.com>
2021-11-11 15:37:52 +0530

    First commit

---
+++ quiz/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+"""class Exam(models.Model):
+    Question = models.CharField(max_length=100)
+    Option1 = models.CharField(max_length=100)
+    Option2 = models.CharField(max_length=100)
+    Option3 = models.CharField(max_length=100)
+    Option4 = models.CharField(max_length=100)
+    Answer = models.CharField(max_length=100)
+
+    #def __str__(self):
+        #return self"""
+
+from django.db import models
+
+
+# Create your models here.
+class QuesModel(models.Model):
+    question = models.CharField(max_length=200, null=True)
+    op1 = models.CharField(max_length=200, null=True)
+    op2 = models.CharField(max_length=200, null=True)
+    op3 = models.CharField(max_length=200, null=True)
+    op4 = models.CharField(max_length=200, null=True)
+    ans = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.question
+
