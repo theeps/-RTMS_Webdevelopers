610cec6c2823d7cb8aaacfa527152dccdb4b818a
Buhary Mohammed Rasith <77905051+rasith1998@users.noreply.github.com>
2021-11-11 18:34:59 +0530

    Update settings.py

--- QuizApp/settings.py
+++ QuizApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quiz',
 
 ]
 
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'QuizApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
+        'NAME': 'quizapp',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
