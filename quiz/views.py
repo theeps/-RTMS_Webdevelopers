fc5ef2f1c75ca6381248f056a8bd18301896f9ad
Buhary Mohammed Rasith <77905051+rasith1998@users.noreply.github.com>
2021-11-12 21:40:08 +0530

    Update views.py

--- quiz/views.py
+++ quiz/views.py
@@ -10,17 +10,95 @@ from django.shortcuts import render
     #exam = Exam.objects.all()
     #return render(request,"home.html",{"exam":exam})
 
-    from django.db import models
+
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout, authenticate
+from .forms import *
+from .models import *
+from django.http import HttpResponse
 
 
-# Create your models here.
-class QuesModel(models.Model):
-    question = models.CharField(max_length=200, null=True)
-    op1 = models.CharField(max_length=200, null=True)
-    op2 = models.CharField(max_length=200, null=True)
-    op3 = models.CharField(max_length=200, null=True)
-    op4 = models.CharField(max_length=200, null=True)
-    ans = models.CharField(max_length=200, null=True)
+# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        print(request.POST)
+        questions = QuesModel.objects.all()
+        score = 0
+        wrong = 0
+        correct = 0
+        total = 0
+        for q in questions:
+            total += 1
+            print(request.POST.get(q.question))
+            print(q.ans)
+            print()
+            if q.ans == request.POST.get(q.question):
+                score += 10
+                correct += 1
+            else:
+                wrong += 1
+        percent = score / (total * 10) * 100
+        context = {
+            'score': score,                       #'time': request.POST.get('timer'),
+            'correct': correct,
+            'wrong': wrong,
+            'percent': percent,
+            'total': total
+        }
+        return render(request, 'result.html', context)
+    else:
+        questions = QuesModel.objects.all()
+        context = {
+            'questions': questions
+        }
+        return render(request, 'Home.html', context)
 
-    def __str__(self):
-        return self.question
+
+def addQuestion(request):
+    if request.user.is_staff:
+        form = addQuestionform()
+        if (request.method == 'POST'):
+            form = addQuestionform(request.POST)
+            if (form.is_valid()):
+                form.save()
+                return redirect('/')
+        context = {'form': form}
+        return render(request, 'addQuestion.html', context)
+    else:
+        return redirect('home')
+
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = createuserform()
+        if request.method == 'POST':
+            form = createuserform(request.POST)
+            if form.is_valid():
+                user = form.save()
+                return redirect('login')
+        context = {
+            'form': form,
+        }
+        return render(request, 'register.html', context)
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+        context = {}
+        return render(request, 'login.html', context)
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('/')
